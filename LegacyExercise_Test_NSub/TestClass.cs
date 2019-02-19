a1f8f866c5837c13d7d52eaee6aa58dda2adf6f3
Mathiasnielsen <au584107@uni.au.dk>
2019-02-19 12:51:11 +0100

    NSubstitute project

---
+++ LegacyExercise_Test_NSub/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS.Legacy;
+using NSubstitute;
+using TESTER;
+
+namespace LegacyExercise_Test_NSub
+{
+    [TestFixture]
+    public class TestClass
+    {
+
+        [Test]
+        public void Test1()
+        {
+            var test1 = Substitute.For<ITempSensor>();
+        }
+    }
+}
